78193796824734fb6ac91b0a378b198fac302256
Rovin Yu <rovinyu@gmail.com>
2018-09-14 09:27:31 +0800

    Add Wear Voice Demo project

---
+++ WearVoiceDemo/app/src/main/java/com/rovin/wearvoicedemo/MainActivity.java
@@ -0,0 +1,47 @@
+package com.rovin.wearvoicedemo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.support.wearable.activity.WearableActivity;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MainActivity extends WearableActivity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextView = (TextView) findViewById(R.id.text);
+
+        // Enables Always-on
+        setAmbientEnabled();
+
+        Intent i = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        i.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        startActivityForResult(i, 0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 0 && resultCode == RESULT_OK) {
+
+            List<String> results = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+
+            String text = results.get(0);
+
+            // Do something with text
+            Log.i("VoiceDemo", "Result: " + text);
+
+        }
+
+    }
+}
